72d8c4737a0e12efcf900bfa67d10eeeb650cedc
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-04 20:44:58 +0100

    Add font-awesome for icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 require("stylesheets/application.scss")
+import "@fortawesome/fontawesome-free/css/all"
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
