c76cfd7a02d58eecb014d2be8d2c2c1ed4ccca27
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-04 21:25:57 +0100

    Add rich text to simple form description in new course

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,3 +21,6 @@ import "@fortawesome/fontawesome-free/css/all"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
